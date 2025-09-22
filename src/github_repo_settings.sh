32d76a289659a2b2fbfa1d76f39b2f03988b25e8
Bhavyai Gupta <zbhavyai@gmail.com>
2025-09-21 21:55:21 -0600

    add creation of branch and tag rulesets in github repo script

--- src/github_repo_settings.sh
+++ src/github_repo_settings.sh
@@ -9,6 +9,8 @@ function settings_general_features() {
         --field has_issues=true \
         --field has_discussions=true \
         --field has_projects=false
+
+    echo "settings_general_features: $?"
 }
 
 function settings_general_pull_requests() {
@@ -23,6 +25,89 @@ function settings_general_pull_requests() {
         --field allow_update_branch=true \
         --field allow_auto_merge=false \
         --field delete_branch_on_merge=true
+
+    echo "settings_general_pull_requests: $?"
+}
+
+function settings_rules_rulesets_branch() {
+    tmp_file=$(mktemp)
+    cat >"${tmp_file}" <<'JSON'
+{
+  "name": "main",
+  "target": "branch",
+  "enforcement": "active",
+  "bypass_actors": [],
+  "conditions": {
+    "ref_name": {
+      "exclude": [],
+      "include": ["~DEFAULT_BRANCH", "refs/heads/main"]
+    }
+  },
+  "rules": [
+    { "type": "creation" },
+    { "type": "deletion" },
+    { "type": "required_linear_history" },
+    {
+      "type": "pull_request",
+      "parameters": {
+        "required_approving_review_count": 0,
+        "dismiss_stale_reviews_on_push": false,
+        "require_code_owner_review": false,
+        "require_last_push_approval": false,
+        "required_review_thread_resolution": false,
+        "automatic_copilot_code_review_enabled": false,
+        "allowed_merge_methods": ["squash"]
+      }
+    },
+    { "type": "non_fast_forward" }
+  ]
+}
+JSON
+
+    gh api "repos/${1}/${2}/rulesets" \
+        --method POST \
+        --silent \
+        --input "${tmp_file}"
+
+    echo "settings_rules_rulesets_branch: $?"
+}
+
+function settings_rules_rulesets_tag() {
+    tmp_file=$(mktemp)
+    cat >"${tmp_file}" <<'JSON'
+{
+  "name": "tag",
+  "target": "tag",
+  "enforcement": "active",
+  "bypass_actors": [
+    {
+      "actor_id": 5,
+      "actor_type": "RepositoryRole",
+      "bypass_mode": "always"
+    }
+  ],
+  "conditions": {
+    "ref_name": {
+      "exclude": [],
+      "include": ["refs/tags/v*.*.*"]
+    }
+  },
+  "rules": [
+    { "type": "creation" },
+    { "type": "update" },
+    { "type": "deletion" },
+    { "type": "required_linear_history" },
+    { "type": "non_fast_forward"}
+  ]
+}
+JSON
+
+    gh api "repos/${1}/${2}/rulesets" \
+        --method POST \
+        --silent \
+        --input "${tmp_file}"
+
+    echo "settings_rules_rulesets_tag: $?"
 }
 
 function main() {
@@ -36,6 +121,8 @@ function main() {
 
     settings_general_features "$owner" "$repo"
     settings_general_pull_requests "$owner" "$repo"
+    settings_rules_rulesets_branch "$owner" "$repo"
+    settings_rules_rulesets_tag "$owner" "$repo"
 }
 
 main "$@"
