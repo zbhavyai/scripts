39ba0f973da82582ad4829b5a36e2cdab857b3af
Bhavyai Gupta <zbhavyai@gmail.com>
2025-10-17 23:57:00 -0600

    add script to list RPM GPG keys

---
+++ src/list_rpm_gpg_keys.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function print_separator() {
+    terminal_width=$(tput cols)
+    printf "%.0s-" $(seq 1 "${terminal_width}")
+    printf "\n"
+}
+
+function list_rpm_gpg_keys() {
+    local atleastOneKey=0
+
+    for key in $(rpm -q gpg-pubkey); do
+        print_separator "-"
+        atleastOneKey=1
+
+        rpm --query "${key}" --queryformat "\
+List name    : ${key}\n\
+Name         : %{name}\n\
+Version      : %{version}\n\
+Release      : %{release}\n\
+Install date : %{installtime:date}\n\
+Packager     : %{packager}\n"
+    done
+
+    if [ $atleastOneKey -eq 0 ]; then
+        :
+    else
+        print_separator "-"
+    fi
+}
+
+function main() {
+    list_rpm_gpg_keys
+}
+
+main
